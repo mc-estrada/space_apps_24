9c47e4fc8aab8725768229ec1b4b977816883085
Richard-Mugisha <mugisharichard2006@gmail.com>
2024-10-06 10:50:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ with open('NEO PHA Database.cvs', 'r') as file:
     csv_reader = csv.reader(file)
     for row in csv_reader:
         print(row)
+with open('Near Earth Comet Database.cvs', 'r') as file:
+    csv_reader2 = csv.reader(file)
+    for row in csv_reader2:
+        print(row)

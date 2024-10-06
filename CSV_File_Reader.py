f2fc63ea0f2cc7acc3e0569542c862dd9543dd7a
Richard-Mugisha <mugisharichard2006@gmail.com>
2024-10-06 12:50:41 -0600

    Update CSV_File_Reader.py

--- CSV_File_Reader.py
+++ CSV_File_Reader.py
@@ -5,15 +5,15 @@ def read_csv(file_name):
         with open(file_name, 'r') as file:
             csv_reader = csv.reader(file)
             for row in csv_reader:
-                print(row)
+                print(f"{row} \n")
     except FileNotFoundError:
         print(f"Error: {file_name} not found.")
     except Exception as e:
         print(f"An error occurred while reading {file_name}: {e}")
 
 def datareader():
-    read_csv('NEO_PHA_Database.csv')
-    read_csv('Near_Earth_Comet_Database.csv')
-    read_csv('Planets_Database.csv')
+    read_csv('NEO PHA Database.csv')
+    read_csv('Near Earth Comet Database.csv')
+    read_csv('Planets Database.csv')
 
 datareader()

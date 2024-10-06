710a42689c3b0f0226e9ee1f42010b158d8494b4
Richard-Mugisha <mugisharichard2006@gmail.com>
2024-10-06 11:17:15 -0600

    Update CVS_File_Reader.py

--- CVS_File_Reader.py
+++ CVS_File_Reader.py
@@ -1,14 +1,17 @@
 import csv
+
 def datareader():
-    with open('NEO PHA Database.cvs', 'r') as file:
+    with open('NEO_PHA_Database.cvs', 'r') as file:
         csv_reader = csv.reader(file)
         for row in csv_reader:
             print(row)
-    with open('Near Earth Comet Database.cvs', 'r') as file:
+    
+    with open('Near_Earth_Comet_Database.cvs', 'r') as file:
         csv_reader2 = csv.reader(file)
         for row in csv_reader2:
             print(row)
-    with open('Planets Database.cvs', 'r') as file:
+    
+    with open('Planets_Database.cvs', 'r') as file:
         csv_reader3 = csv.reader(file)
         for row in csv_reader3:
             print(row)
